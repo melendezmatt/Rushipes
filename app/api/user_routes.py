18ef3e04c7519b748dd65d24465b9785576ee99d
Matt Melendez <matthewemelendez@gmail.com>
2021-07-28 09:40:09 -0700

    started pantry backend

--- app/api/user_routes.py
+++ app/api/user_routes.py
@@ -1,9 +1,8 @@
 from flask import Blueprint, jsonify
 from flask_login import login_required
-from app.models import User
+from app.models import User, Pantry
 
 user_routes = Blueprint('users', __name__)
-
 
 @user_routes.route('/')
 @login_required
@@ -17,3 +16,30 @@ def users():
 def user(id):
     user = User.query.get(id)
     return user.to_dict()
+
+#Get All Pantries ['GET']
+@user_routes.route('/<int:id>/all-pantries', methods=['GET'])
+def get_all_pantries(id):
+    pantries = Pantry.query.filter(Pantry.user_id == id).all()
+    return { 'pantries': [pantry.to_dict() for pantry in pantries]}
+
+#Get Single Pantry ['GET']
+@user_routes.route('/<int:id>/pantry/<int:pantryId>', methods=['GET'])
+def get_single_pantry(id, pantryId):
+    pantry = Pantry.query.filter(Pantry.user_id == id and Pantry.id == pantryId)
+    return pantry.to_dict()
+
+#Create Single Pantry ['POST']
+@user_routes.route('/<int:id>/new-pantry/', methods=['POST'])
+
+
+
+#Update Single Pantry ['PUT']
+@user_routes.route('/<int:id>/pantry/<int:pantryId>', methods=['PUT'])
+
+
+
+#Delete Single Pantry ['DELETE']
+@user_routes.route('/<int:id>/pantry/<int:pantryId>', methods=['DELETE'])
+def delete_single_pantry(id, pantryId):
+    pantry = Pantry.query.filter(Pantry.user_id == id and Pantry.id == pantryId)
--- app/models/user.py
+++ app/models/user.py
@@ -11,6 +11,11 @@ class User(db.Model, UserMixin):
     email = db.Column(db.String(255), nullable=False, unique=True)
     hashed_password = db.Column(db.String(255), nullable=False)
 
+    pantry = db.relationship(
+        "Pantry",  uselist=False,
+        back_populates="user"
+    )
+
     @property
     def password(self):
         return self.hashed_password
