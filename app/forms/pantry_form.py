d9d4bd1e190124a92e368147d9c06b03d4d21e82
Matt Melendez <matthewemelendez@gmail.com>
2021-08-07 22:55:16 -0700

    fixed pantry form error message

--- app/forms/pantry_form.py
+++ app/forms/pantry_form.py
@@ -6,4 +6,4 @@ class PantryForm(FlaskForm):
     pantry_name=StringField('pantry_name', validators=[DataRequired('Please provide a name for this pantry.'), Length(max=40, message="The pantry name provided is too long.")])
     pantry_image_url=StringField('pantry_image_url', validators=[Length(max=255, message="The URL provided is too long.")])
     location=StringField('location', validators=[DataRequired('Please provide a location for this pantry.'), Length(max=255, message="The location provided is too long.")])
-    about=StringField('about', validators=[DataRequired('Please provide text about this pantry.'), Length(max=500, message="The description provided is too long.")])
+    about=StringField('about', validators=[DataRequired('Please provide a description for this pantry.'), Length(max=500, message="The description provided is too long.")])
